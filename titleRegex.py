89746af4f9e850d6973fa7a6c9c37a915db96256
JamesNix <james@visitmusiccity.com>
2019-05-16 14:38:03 -0500

    Remove unecessary code

--- titleRegex.py
+++ titleRegex.py
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 import re
-import subprocess
-from subprocess import Popen, PIPE, STDOUT
 
-list = open("./businessTitlesShort.txt", "r")
+list = open("./businessTitles.txt", "r")
 newList = open("./newList.txt", "w")
 
 articles = re.compile(r"\bof \b|\bby \b|\bat \b|\bthe \b|\bat \b|\ban \b|\bas \b|\bbefore \b|\bbut \b|\bby \b|\bfor \b|\bfrom \b|\bis \b|\bin \b|\binto \b|\blike \b|\w\bof\b\w|\boff \b|\bon \b|\bonto \b|\bper \b|\bsince \b|\bthan \b|\bthis \b|\bthat \b|\bto \b|\bup \b|\bvia \b|\bwith \b|[\.\(\):\!'+|&@\/]", re.IGNORECASE)
@@ -15,15 +13,14 @@ def writeNewList(t):
     newList.write(t + "\n")
     return
 
-with open("businessTitles.txt") as list:
-  for line in list:
-      orgLine = line.split("\n")[0]
-      title = line.split(",", 1)[1]
-      newTitle = articles.sub("", title)
-      newTitle = spaces.sub("-", newTitle)
-      newTitle = dashes.sub("-", newTitle)
-      newLine = orgLine + "," + newTitle[:-1]
-      writeNewList(newLine)
+for line in list:
+  orgLine = line.split("\n")[0]
+  title = line.split(",", 1)[1]
+  newTitle = articles.sub("", title)
+  newTitle = spaces.sub("-", newTitle)
+  newTitle = dashes.sub("-", newTitle)
+  newLine = orgLine + "," + newTitle[:-1]
+  writeNewList(newLine)
 
 list.close()
 newList.close()
